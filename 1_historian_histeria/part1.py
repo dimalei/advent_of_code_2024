546717ec99d89a138e959e02178b116509e50ca2
dimalei <dl+github@ava.com>
2024-12-03 15:15:32 +0100

    day 1 part 2

--- 1_historian_histeria/part1.py
+++ 1_historian_histeria/part1.py
@@ -1,11 +1,12 @@
-def getInputLists(filename = "input.txt"):
+def getInputLists(filename="input.txt"):
     l1, l2 = [], []
     with open(filename, "r") as f:
         for line in f:
-            a,b = line.strip().split("   ")
+            a, b = line.strip().split("   ")
             l1.append(int(a))
             l2.append(int(b))
     return (l1, l2)
+
 
 def get_distance(a: list, b: list) -> int:
     a.sort()
@@ -17,8 +18,22 @@ def get_distance(a: list, b: list) -> int:
     return total_distance
 
 
-if(__name__) == "__main__":
+def similarity_score(a: list, b: list) -> int:
+    score = 0
+    for id_a in a:
+        appearances = 0
+        for id_b in b:
+            if (id_a == id_b):
+                appearances += 1
+        score += id_a * appearances
+    return score
 
+
+if (__name__) == "__main__":
+    # test list
     # a, b = getInputLists("test_input.txt")
+    # real list
     a, b = getInputLists()
-    print(get_distance(a,b))
+
+    print(get_distance(a, b))
+    print(similarity_score(a, b))
