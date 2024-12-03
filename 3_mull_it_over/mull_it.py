7c309f312294b5c569b63bd0a1d9bd21973471b2
dimalei <dima.leigr@gmail.com>
2024-12-03 23:18:10 +0100

    refactor

--- 3_mull_it_over/mull_it.py
+++ 3_mull_it_over/mull_it.py
@@ -1,37 +1,39 @@
 import re
 
+
 def getInputData(filename="input.txt"):
     with open(filename, "r") as f:
         return f.read()
 
-def calculate_mul(input: str) -> int:
-    if not re.fullmatch("mul\(\d+,\d+\)", match):
+
+def calculate_mul(matches: str) -> int:
+    if not re.fullmatch("mul\(\d+,\d+\)", matches):
         return 0
-    a,b = input.split(",")
-    a = int(a[4:])
-    b = int(b[:-1])
-    return a * b
+    a, b = re.findall("\d+", matches)
+    return int(a) * int(b)
 
-if __name__ == "__main__":
-    # data = getInputData("test_input.txt")
-    data = getInputData()
-    mul_matches = re.findall("(mul\(\d+,\d+\))", data)
-    sum = 0
-    for match in mul_matches:
-        sum += calculate_mul(match)
-    print(sum)
 
-    mul_do_dont_matches = re.findall("mul\(\d+,\d+\)|do\\(\)|don't\(\)", data)
+def calculate_matches(matches: str) -> int:
     do = True
-    sum2 = 0
-    for match in mul_do_dont_matches:
+    sum = 0
+    for match in matches:
         if match == 'do()':
             do = True
         elif match == "don't()":
             do = False
         elif re.fullmatch("mul\(\d+,\d+\)", match) and do:
-            sum2 += calculate_mul(match)
-    print(sum2)
+            sum += calculate_mul(match)
+    return sum
 
 
+if __name__ == "__main__":
+    # data = getInputData("test_input.txt")
+    data = getInputData()
 
+    mul_matches = re.findall("(mul\(\d+,\d+\))", data)
+    sum = calculate_matches(mul_matches)
+    print(f'part1: {sum}')
+
+    mul_do_dont_matches = re.findall("mul\(\d+,\d+\)|do\\(\)|don't\(\)", data)
+    sum2 = calculate_matches(mul_do_dont_matches)
+    print(f'part2: {sum2}')
