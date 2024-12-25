eb2cd1b5c7928390030fcf1bf689172065b5aa44
dimalei <dima.leigr@gmail.com>
2024-12-25 14:05:02 +0100

    to laptop

--- 16_reindeer_maze/reindeer_maze.py
+++ 16_reindeer_maze/reindeer_maze.py
@@ -27,34 +27,43 @@ class Vector2:
             return False
         return self._x == other._x and self._y == other._y
 
+    def __str__(self):
+        return f"({self._x},{self._y})"
 
-class Direction(Enum):
+
+class Heading(Enum):
     NORTH = Vector2(0, -1)
     EAST = Vector2(1, 0)
     SOUTH = Vector2(0, 1)
     WEST = Vector2(-1, 0)
 
     def turn_cw(self):
-        directions = list(Direction)
+        directions = list(Heading)
         current_index = directions.index(self)
         next_index = (current_index + 1) % len(directions)
         return directions[next_index]
 
     def turn_ccw(self):
-        directions = list(Direction)
+        directions = list(Heading)
         current_index = directions.index(self)
         next_index = (current_index - 1) % len(directions)
         return directions[next_index]
 
     def __str__(self):
-        if self == Direction.NORTH:
+        if self == Heading.NORTH:
             return "^"
-        if self == Direction.EAST:
+        if self == Heading.EAST:
             return ">"
-        if self == Direction.SOUTH:
+        if self == Heading.SOUTH:
             return "v"
-        if self == Direction.WEST:
+        if self == Heading.WEST:
             return "<"
+
+
+class Direction(Enum):
+    LEFT = 0
+    RIGHT = 1
+    STRAIGHT = 2
 
 
 class Maze:
@@ -64,7 +73,9 @@ class Maze:
         self.exit = Vector2(0, 0)
         self.parse_input(file_name)
         self.player_pos = self.start
-        self.player_heading = Direction.EAST
+        self.player_heading = Heading.EAST
+        self.trails = []
+        self.lowest_cost = 0
 
     def parse_input(self, file_name):
         with open(file_name, "r") as file:
@@ -77,10 +88,48 @@ class Maze:
                     elif char == "S":
                         self.start = (Vector2(x, y))
 
-    # def see_ahead(self):
-    #     left_location = Path(self.player_pos + self.player_heading.turn_ccw())
+    def see_ahead(self) -> list:
+        directions = []
 
-    #     return left
+        if self.player_pos + self.player_heading.turn_ccw().value in self.paths:
+            directions.append(Direction.LEFT)
+        if self.player_pos + self.player_heading.value in self.paths:
+            directions.append(Direction.STRAIGHT)
+        if self.player_pos + self.player_heading.turn_cw().value in self.paths:
+            directions.append(Direction.RIGHT)
+
+        return directions
+
+    def create_trail(self):
+        trail = {}
+        cost = 0
+        self.player_pos = self.start
+        self.player_heading = Heading.EAST
+        directions = self.see_ahead()
+
+        while len(directions) > 0 and not self.player_pos in trail.keys():
+            trail[self.player_pos] = self.player_heading
+            # go left
+            if Direction.LEFT in directions:
+                cost += 1001
+                self.player_heading = self.player_heading.turn_ccw()
+                self.player_pos += self.player_heading.value
+            # go straight
+            elif Direction.STRAIGHT in directions:
+                cost += 1
+                self.player_pos += self.player_heading.value
+            # go right
+            elif Direction.RIGHT in directions:
+                cost += 1001
+                self.player_heading = self.player_heading.turn_cw()
+                self.player_pos += self.player_heading.value
+
+            else:
+                break
+
+            directions = self.see_ahead()
+
+        self.trails.append(trail)
 
     def __str__(self):
         out = ""
@@ -92,15 +141,15 @@ class Maze:
 
                 location = Vector2(x, y)
 
-                if location == self.player_pos:
-                    out += self.player_heading.__str__()
+                if len(self.trails) > 0 and location in self.trails[-1]:
+                    out += self.trails[-1][location].__str__()
 
+                elif location == self.player_pos:
+                    out += self.player_heading.__str__()
                 elif location in self.paths:
                     out += "."
-
                 elif location == self.start:
                     out += "S"
-
                 elif location == self.exit:
                     out += "E"
 
@@ -115,7 +164,8 @@ if __name__ == "__main__":
     maze = Maze()
 
     print(maze)
-    print(maze.see_ahead())
+    print(maze.create_trail())
+    print(maze)
 
     # path1 = Path(Vector2(12, 13))
     # path2 = Exit(Vector2(12, 13))
