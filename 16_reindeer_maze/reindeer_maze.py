71422346fd9b9a0e343589ca89425b66b52ffe11
dimalei <dima.leigr@gmail.com>
2024-12-26 12:05:29 +0100

    node can grow nodes

--- 16_reindeer_maze/reindeer_maze.py
+++ 16_reindeer_maze/reindeer_maze.py
@@ -69,12 +69,14 @@ class Direction(Enum):
 class Node:
     def __init__(self, root: 'Node', pos: Vector2, heading: Heading, branches: dict, trail: list, cost: int):
         self.root = root
+        self.pos = pos
+        self.heading = heading
         self.branches = branches
         self.trail = trail
         self.cost = cost
-        self.heading = heading
-        self.pos = pos
 
+    def __repr__(self):
+        return f"Node((parent), {self.pos}, {self.heading}, branches: {len(self.branches)}, trail: {len(self.trail)}, cost: {self.cost})"
 
 class Maze:
     def __init__(self, file_name="test_input.txt"):
@@ -82,12 +84,12 @@ class Maze:
         self.start = Vector2(0, 0)
         self.exit = Vector2(0, 0)
         self.parse_input(file_name)
-        self.player_pos = self.start
-        self.player_heading = Heading.EAST
+        self.root_node = self.create_root()
         self.trails = []
         self.lowest_cost = 0
-        self.root_note = None
         self.exit_nodes = []
+        print(self.start)
+        print(self.root_node)
 
     def parse_input(self, file_name):
         with open(file_name, "r") as file:
@@ -102,85 +104,77 @@ class Maze:
                         self.start = (Vector2(x, y))
                         self.paths.append(self.start)
 
-    def see_ahead(self) -> list:
-        directions = []
-        if self.player_pos + self.player_heading.turn_ccw().value in self.paths:
-            directions.append(Direction.LEFT)
-        if self.player_pos + self.player_heading.value in self.paths:
-            directions.append(Direction.STRAIGHT)
-        if self.player_pos + self.player_heading.turn_cw().value in self.paths:
-            directions.append(Direction.RIGHT)
+    def create_root(self):
+        directions = self.see_ahead(self.start, Heading.EAST)
+        branches = {}
+        for direction in directions:
+            branches[direction] = None
+        return Node(None, self.start, Heading.EAST, branches, [], 0)
 
+    def see_ahead(self, player_pos: Vector2, player_heading: Heading) -> list:
+        directions = []
+        if player_pos + player_heading.turn_ccw().value in self.paths:
+            directions.append(Direction.LEFT)
+        if player_pos + player_heading.value in self.paths:
+            directions.append(Direction.STRAIGHT)
+        if player_pos + player_heading.turn_cw().value in self.paths:
+            directions.append(Direction.RIGHT)
         return directions
 
-    def create_tree(self):
-        cost = 0
-        trail = []
-        self.player_pos = self.start
-        self.player_heading = Heading.EAST
-        directions = self.see_ahead()
+    def extend_node(self, node: Node):
+        for branch in node.branches.keys():
+            node.branches[branch] = self.extend_branch(node, branch)
+            print(f"node created: {node.branches[branch]}")
 
-        # follow path until next junction (node)
+    def extend_branch(self, node: Node, first_direction: Direction) -> Node:
+        cost = node.cost
+        trail = node.trail
+        player_pos = node.pos
+        player_heading = node.heading
+
+        # commit to first direction
+        if first_direction == Direction.LEFT:
+            cost += 1001
+            player_heading = player_heading.turn_ccw()
+        elif first_direction == Direction.STRAIGHT:
+            cost += 1
+        elif first_direction == Direction.RIGHT:
+            cost += 1001
+            player_heading = player_heading.turn_cw()
+
+        player_pos += player_heading.value
+        trail.append(player_pos)
+
+        # then follow path
+        directions = self.see_ahead(player_pos, player_heading)
         while len(directions) == 1:
             if Direction.LEFT in directions:
                 cost += 1001
-                self.player_heading = self.player_heading.turn_ccw()
+                player_heading = player_heading.turn_ccw()
             # go straight
             elif Direction.STRAIGHT in directions:
                 cost += 1
             # go right
             elif Direction.RIGHT in directions:
                 cost += 1001
-                self.player_heading = self.player_heading.turn_cw()
+                player_heading = player_heading.turn_cw()
 
-            self.player_pos += self.player_heading.value
-            trail.append(self.player_pos)
-            directions = self.see_ahead()
+            player_pos += player_heading.value
+            trail.append(player_pos)
+            directions = self.see_ahead(player_pos, player_heading)
 
+            print(player_pos)
+
+        # retrun none at dead end
+        if len(directions) == 0:
+            return None
+
+        # at the junction, create the new node
         branches = {}
         for direction in directions:
             branches[direction] = None
 
-        # create a node and follow left most option
-        self.root_note = Node(None, self.player_pos, self.player_heading, branches, trail, cost)
-
-    def grow_branch(self, node: Node):
-        # define start direction
-        for branch in node.branches:
-            pass
-
-
-
-
-    def create_trail(self):
-        trail = {}
-        cost = 0
-        self.player_pos = self.start
-        self.player_heading = Heading.EAST
-        directions = self.see_ahead()
-
-        while len(directions) > 0 and not self.player_pos in trail.keys():
-            trail[self.player_pos] = self.player_heading
-            # go left
-            if Direction.LEFT in directions:
-                cost += 1001
-                self.player_heading = self.player_heading.turn_ccw()
-                self.player_pos += self.player_heading.value
-            # go straight
-            elif Direction.STRAIGHT in directions:
-                cost += 1
-                self.player_pos += self.player_heading.value
-            # go right
-            elif Direction.RIGHT in directions:
-                cost += 1001
-                self.player_heading = self.player_heading.turn_cw()
-                self.player_pos += self.player_heading.value
-            else:
-                break
-
-            directions = self.see_ahead()
-
-        self.trails.append(trail)
+        return Node(None, player_pos, player_heading, branches, trail.copy(), cost)
 
     def __str__(self):
         out = ""
@@ -215,26 +209,8 @@ if __name__ == "__main__":
     maze = Maze()
 
     # print(maze)
-    # maze.create_tree()
+    maze.extend_node(maze.root_node)
     # print(maze)
-
-
-    
-
-    direction_dict = {Direction.LEFT: Node(None,None,None,None,None,None),
-                      Direction.STRAIGHT: None,
-                      Direction.RIGHT: None}
-    
-    # return first empty
-    next_direction = None
-    for key in direction_dict.keys():
-        if direction_dict[key] == None:
-            next_direction = key
-            break
-    
-    print(next_direction)
-
-
 
     # path1 = Path(Vector2(12, 13))
     # path2 = Exit(Vector2(12, 13))
